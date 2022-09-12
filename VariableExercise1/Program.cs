8969069f2727dc4b9bba5e8eb77eb06797569161
earl starks <earlstarks@rocketmail.com>
2022-09-12 15:00:13 -0500

    VariableExcercise1

---
+++ VariableExercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Hunter";
+            int age = 4;
+            int weight = 8;
+            bool isMale= true;
+
+            Console.WriteLine($"My dog's name is {dogName}, He is {age} years old, He is {weight} pounds, He {isMale}");
+
+
+        }
+    }
+}
+
